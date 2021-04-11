96cf6cc85ac84e1507aca384b2d863782c21b6d6
jomsanchez <jomsanchez@ucdavis.edu>
2021-04-11 09:27:47 -0700

    Update WheelSpeed.m

--- Vehicle Model Resources/WheelSpeed.m
+++ Vehicle Model Resources/WheelSpeed.m
@@ -1,5 +1,5 @@
-function [InputTorque, SpinAcc] = WheelSpeed( SpinRate, ...
-    DriveTorque, BrakeTorque, TractiveForce, EffRadius, Inertia, Damping)
+function [InputTorque, SpinAcc] = WheelSpeed( SpinRate,DriveTorque, ...
+    BrakeTorque, TractiveForce, EffRadius, Inertia, Damping, DifferentialEffec )
 
 %% WheelSpeed - Wheel Torque Balance & Spin Acceleration
 % Calculates brake or drive torque required for steady state applications
@@ -24,23 +24,49 @@ function [InputTorque, SpinAcc] = WheelSpeed( SpinRate, ...
 %
 % Author(s): 
 % Blake Christierson (bechristierson@ucdavis.edu) [Sep 2018 - Jun 2021] 
-%
-% Last Updated: 7-Apr-2021
+%Joseph Sanchez (jomsanchez@ucdavis.edu) [Sep 2020 - June 2022]
+% Last Updated: 10-Apr-2021
 
 %%% Test Cases
 if nargin == 0
     %%% Test Inputs
-    SpinRate = 80;
+    SpinRate = 80*ones(1,4); 
     
-    DriveTorque = 20;
-    BrakeTorque = 0;
+    DriveTorque = 20*ones(1,4); 
+    BrakeTorque = 0*ones(1,4); 
     
-    TractiveForce = 300;
+    TractiveForce = 300*ones(1,4);
     EffRadius     = 0.19;
     
-    Inertia = 
-    Damping = 
+    Inertia = 1;
+    Damping = 1;
+    DifferentialEffec = 1; 
+ 
+    [InputTorque, SpinAcc] = WheelSpeed( SpinRate, DriveTorque, BrakeTorque, ...
+        TractiveForce, EffRadius, Inertia, Damping, DifferentialEffec);
+
+    fprintf('Executing WheelSpeed() Test Case: \n');
+    for i = 1:numel(SpinAcc)
+        fprintf('   Instance %i: \n', i);
+        fprintf('      omega_dot = %5.2f [rad/s^2] \n', SpinAcc(i));
+
+    end
+    
+     return;   
 end
+
+n = numel(SpinRate);
+SpinAcc = zeros(1,n);
+
+for i = 1:n
+
+SpinAcc(1:i) = (DifferentialEffec*DriveTorque(1:i) - ...
+    TractiveForce(1:i)*EffRadius - BrakeTorque(1:i) - ...
+    SpinRate(1:i)*Damping).*(1/Inertia);
+
+end
+
+InputTorque = 1;
 
 end
 
